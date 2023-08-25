9688bdb32b36b6cfc00832c205dbc15082223623
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-08-25 13:57:33 +0300

    Update ReplaceWords.py

--- ReplaceWords.py
+++ ReplaceWords.py
@@ -46,7 +46,7 @@ class ReplaceWords(loader.Module):
     async def watcher(self,message):
         words = self.get("words",[])
         me = await self.client.get_me(id)
-        user_id = me.id
+        user_id = me.user_id
         if message.text.lower() in words and self.config["status"] and message.from_id == user_id:
             word = self.get(message.text.lower(),None)
             await utils.answer(message,word)
