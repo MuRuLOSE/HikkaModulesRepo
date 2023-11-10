0762802b0bc29fbaee4b5ae6496702deaa14575b
Macsim <podryazhchik@mail.ru>
2023-11-10 18:04:48 +0300

    bruh i not commited anything

---
+++ !example.py
@@ -0,0 +1,22 @@
+from hikkatl.types import Message
+from .. import loader, utils
+
+# meta developer: @BruhHikkaModules
+
+@loader.tds
+class MyModule(loader.Module):
+    """My module"""
+    strings = {"name": "MyModule", "hello": "Hello world!"}
+    strings_ru = {"hello": "Привет мир!"}
+    strings_es = {"hello": "¡Hola mundo!"}
+    strings_de = {"hello": "Hallo Welt!"}
+
+    @loader.command(
+        ru_doc="Привет мир!",
+        es_doc="¡Hola mundo!",
+        de_doc="Hallo Welt!",
+        # ...
+    )
+    async def helloworld(self, message: Message):
+        """Hello world"""
+        await utils.answer(message, self.strings("hello"))
---
+++ autogiveawayjoin.py
@@ -0,0 +1,47 @@
+from hikkatl.types import Message
+from .. import loader, utils
+
+import asyncio
+import random
+
+# meta developer: @BruhHikkaModules
+
+
+@loader.tds
+class AutoGiveawayJoin(loader.Module):
+    """Авто присоеденение к розыгрышам в @mine_evo_bot"""
+    strings = {"name": "MyModule", "hello": "Hello world!"}
+    strings_ru = {"hello": "Привет мир!"}
+    strings_es = {"hello": "¡Hola mundo!"}
+    strings_de = {"hello": "Hallo Welt!"}
+
+    def __init__(self):
+        self.bot = 5522271758
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "join",
+                60,
+                "Присоеденяться к розыгрышам?",
+                validator=loader.validators.Boolean()
+            )
+        )
+
+    @loader.watcher(from_id=5522271758)
+    async def join_giveaway(self, message: Message):
+
+        if message.text == "Success start!":
+            await asyncio.sleep(random.randint(2, 5))
+            await self.client.send_message(self.bot, "Участвую")
+
+    @loader.command()
+    async def giveawayjoin(self, message: Message):
+        """ - Вкл / Выкл присоеденение к розыгрышам"""
+        self.config["join"] = not self.config["join"]
+
+        status = (
+            "Авто присоеденение включено"
+            if self.config["join"]
+            else "Авто присоеденение выключено"
+        )
+
+        await utils.answer(message, status)
