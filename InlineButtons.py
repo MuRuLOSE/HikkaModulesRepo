0d7de8cf52a1d5cd37457bbdeeffb34523c1f59f
Macsim <podryazhchik@mail.ru>
2023-12-19 16:51:20 +0300

    about

---
+++ InlineButtons.py
@@ -0,0 +1,48 @@
+from hikkatl.types import Message
+from .. import loader, utils
+
+'''
+    ███    ███ ██    ██ ██████  ██    ██ ██       ██████  ███████ ███████
+    ████  ████ ██    ██ ██   ██ ██    ██ ██      ██    ██ ██      ██  
+    ██ ████ ██ ██    ██ ██████  ██    ██ ██      ██    ██ ███████ █████
+    ██  ██  ██ ██    ██ ██   ██ ██    ██ ██      ██    ██      ██ ██  
+    ██      ██  ██████  ██   ██  ██████  ███████  ██████  ███████ ███████ 
+
+
+    InlineButtons
+    📜 Licensed under the GNU AGPLv3	
+'''
+
+# meta banner: link
+# meta desc: desc
+# meta developer: @BruhHikkaModules
+
+
+@loader.tds
+class InlineButtons(loader.Module):
+    """Create inline buttons easily"""
+
+    strings = {"name": "InlineButtons"}
+    strings_ru = {"_cls_doc": "Создайте инлайн кнопки легко"}
+
+    @loader.command(
+        ru_doc=" [Ссылка на изображение (опционально)] [Текст кнопки] [Ссылка в кнопке] [Текст] - Создать инлайн кнопку",
+    )
+    async def cinline(self, message: Message):
+        """ [Button text] [Button link] [Text] - Create inline button"""
+        args = utils.get_args_raw(message).split()
+
+        await self.inline.form(
+            text=' '.join(args[2:]),
+            message=message,
+            reply_markup=[
+                [
+                    {
+                        "text": args[0],
+                        "url": args[1]
+                    }
+                ]
+            ]
+        )
+
+
