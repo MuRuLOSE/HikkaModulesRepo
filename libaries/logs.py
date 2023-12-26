7c5d42df5dc3edd7ad4f9821000e01c3bd47a79b
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-12-26 17:37:40 +0300

    Update logs.py

--- libaries/logs.py
+++ libaries/logs.py
@@ -1,1 +1,27 @@
-# maybe i write logging libary
+from .. import loader
+import random
+import logging
+
+
+class LogHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+
+    def emit(self, record):
+        send_log(record)
+
+class BHikkamodsLogsLib(loader.Library):
+    developer = "@MuRuLOSE"
+
+
+    def __init__(self):
+        self.config = loader.LibraryConfig(
+            loader.ConfigValue(
+                "send_errors",
+                False,
+                "Send errors to developer for diagnostic",
+                validator=loader.validators.String(),
+            )
+        )
+        
+        self._log_handler = LogHandler()
