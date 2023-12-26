ad888e691f8e71c771aa620177f756f3f8fee955
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-12-26 17:52:58 +0300

    Update logs.py

--- libaries/logs.py
+++ libaries/logs.py
@@ -3,12 +3,28 @@ import random
 import logging
 
 
-class LogHandler(logging.Handler):
+class LogHandler(logging.Handler, BHikkamodsLogsLib):
     def __init__(self):
         super().__init__()
+        
+    async def send_log(self, record):
+        send_id = False
+        
+        if self.config['send_id']:
+            send_id = True
+            
+        await self.client.send_message(
+            '@murulose_hikka_exceptionsbot', 
+            (
+                record + f'\n\n{await (self.get_me()).id}'
+                if send_id
+                else record
+            )
+        )
 
     def emit(self, record):
-        send_log(record)
+        if self.config["send_errors"]:
+            await self.send_log(record)
 
 class BHikkamodsLogsLib(loader.Library):
     developer = "@MuRuLOSE"
@@ -21,7 +37,13 @@ class BHikkamodsLogsLib(loader.Library):
                 False,
                 "Send errors to developer for diagnostic",
                 validator=loader.validators.String(),
+            ),
+            loader.ConfigValue(
+                "send_id"
+                False,
+                "Send id with error"
             )
         )
+        
         
         self._log_handler = LogHandler()
