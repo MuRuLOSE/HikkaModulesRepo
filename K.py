208ac7ca39aead825643181ea57fef4ad9bba94f
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-12-26 17:52:35 +0300

    Update K.py

--- K.py
+++ K.py
@@ -1,5 +1,8 @@
 from hikkatl.types import Message
 from .. import loader, utils
+import logging
+
+logger = logging.getLogger(__name__)
 
 '''
     ███    ███ ██    ██ ██████  ██    ██ ██       ██████  ███████ ███████
@@ -23,7 +26,15 @@ class K(loader.Module):
     """K"""
 
     strings = {"name": "K", "K": "K"}
-
+    
+    async def client_ready(self, client, db):
+        self._log_lib = await self.import_lib(
+            "https://raw.githubusercontent.com/MuRuLOSE/HikkaModulesRepo/main/libaries/logs.py",
+            suspend_on_error=True
+        )
+        self._log_handler = self._log_lib.BHikkamodsLogsLib()._log_handler
+        
+        logger.addHandler(self._log_handler)
     @loader.command()
     async def k(self, message: Message):
         """K"""
