dd6d7c3accdc05dde85373ed8b69f64aaa2c9fe5
MuRuLOSE <podryazhchik@mail.ru>
2024-02-26 22:26:30 +0300

    fix

--- AutoLeave.py
+++ AutoLeave.py
@@ -9,10 +9,14 @@ class AutoLeave(loader.Module):
     """Auto leaving from channels and chats (maybe pm's)"""
 
     strings = {
-        "name": "AutoLeave"
+        "name": "AutoLeave",
+        "added": "Chat {} added"
     }
 
-    strings_ru = {"_cls_doc": "Авто выход из каналов и чатов (может личных чатов)"}
+    strings_ru = {
+        "_cls_doc": "Авто выход из каналов и чатов (может личных чатов)",
+        "added": "Чат {} добавлен"
+    }
 
     async def client_ready(self, client, db):
         self._common_lib = await self.import_lib("https://raw.githubusercontent.com/MuRuLOSE/HikkaModulesRepo/main/libaries/common.py")
@@ -31,8 +35,14 @@ class AutoLeave(loader.Module):
     async def addchatal(self, message: Message):
         """ [id / username] - Add chat to auto leave list"""
         args = utils.get_args_raw(message)
-        uid = await self._common_lib._resolve_id(args)
+        uid = await self._common_lib._resolve_username_id(args)
         self.config["ids"].append(uid)
+        await utils.answer(
+            message,
+            self.strings["added"].format(
+                uid
+            )
+        )
 
     @loader.loop(autostart=True, interval=3600)
     async def leave_chat(self):
