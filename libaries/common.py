670a1fbad3fee2218df23bf054fa1ee37bfd364c
MuRuLOSE <podryazhchik@mail.ru>
2024-02-26 20:16:22 +0300

    bro it's real, user id resolve by username

--- libaries/common.py
+++ libaries/common.py
@@ -6,8 +6,11 @@ class CommonLib(loader.Library):
     '''Lib for modules with common items, like topic resolver.'''
     developer = "@MuRuLOSE"
 
-    async def _topic_resolver(self, message: Message):
+    async def _topic_resolver(self, message: Message) -> int:
         if message.reply_to.forum_topic:
             topic_id = message.reply_to.reply_to_top_id
             return topic_id
 
+    async def _resolve_username_id(self, username: str) -> int:
+        data = await self.client.get_entity(username)
+        return data.id
